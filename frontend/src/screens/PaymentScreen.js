14bd3c992c6a6b16c0b99a30c1cf5ebc351b700c
saltysam14 <92763644+saltysam14@users.noreply.github.com>
2022-08-28 00:47:43 -0400

    4

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -41,14 +41,14 @@ const PaymentScreen = () => {
               value="PayPal"
               onChange={(e) => setPaymentMethod(e.target.value)}
             ></Form.Check>
-            <Form.Check
+            {/* <Form.Check
               type="radio"
               label="Stripe"
               id="Stripe"
               name="paymentMethod"
               value="Stripe"
               onChange={(e) => setPaymentMethod(e.target.value)}
-            ></Form.Check>
+            ></Form.Check> */}
           </Col>
         </Form.Group>
 
